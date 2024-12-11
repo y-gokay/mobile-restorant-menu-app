f8d190c3052988903a185b085bfee4a182a3a844
Yasin Gokay <ygokaykokdemir@outlook.com>
2024-12-11 20:55:58 +0300

    Mobile-Restorant-Menu-App

---
+++ App.js
@@ -0,0 +1,87 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FoodOverviewScreen from "./screens/FoodOverviewScreen";
+import FoodDetailScreen from "./screens/FoodDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
+import FavoritesContextProvider from "./store/favoritescontext";
+import { Provider } from "react-redux";
+import { store } from "./store/redux/store";
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "white" },
+        headerTintColor: "red",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "Tüm Kategoriler",
+          drawerIcon: () => <Ionicons name="list" size={24} color="black" />,
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          title: "Favoriler",
+          drawerIcon: () => (
+            <Ionicons name="ios-star-half" size={24} color="black" />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "white" },
+            headerTintColor: "red",
+            contentStyle: { backgroundColor: "lightgrey" },
+          }}
+        >
+          <Stack.Screen
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="FoodOverview" component={FoodOverviewScreen} />
+          <Stack.Screen
+            name="FoodDetail"
+            component={FoodDetailScreen}
+            options={{
+              title: "İçerik",
+            }}
+          />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screens/FoodDetailScreen.js
@@ -0,0 +1,124 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Pressable,
+} from "react-native";
+import React from "react";
+import { useLayoutEffect, useContext } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+import { FOODS } from "../data/dummy-data";
+import FoodIngredients from "../components/FoodIngredients";
+import { FavoritesContext } from "../store/favoritescontext";
+import { useSelector, useDispatch } from "react-redux";
+import { addFavorite, removeFavorite } from "../store/redux/favorites";
+
+export default function FoodDetailScreen({ route, navigation }) {
+  const favoriteFoodsIds = useSelector((state) => state.favoriteFoods.ids);
+
+  const foodId = route.params.foodId;
+
+  const dispatch = useDispatch();
+  const selectedFood = FOODS.find((food) => food.id === foodId);
+
+  const foodIsFavorite = favoriteFoodsIds.includes(foodId);
+
+  const pressHandler = () => {
+    console.log("Tıklandı!");
+  };
+
+  function changeFavorite() {
+    if (foodIsFavorite) {
+      dispatch(removeFavorite({ id: foodId }));
+    } else {
+      dispatch(addFavorite({ id: foodId }));
+    }
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Pressable
+            onPress={pressHandler}
+            style={({ pressed }) => (pressed ? styles.pressed : null)}
+          >
+            <Ionicons
+              name={foodIsFavorite ? "star" : "star-outline"}
+              size={24}
+              color="red"
+              onPress={changeFavorite}
+            />
+          </Pressable>
+        );
+      },
+    });
+  }, [navigation, changeFavorite]);
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedFood.imageUrl }} />
+      <Text style={styles.title}>{selectedFood.title}</Text>
+      <View style={styles.details}>
+        <Text style={styles.detailItem}>{selectedFood.complexity}</Text>
+        <Text style={styles.detailItem}>{selectedFood.affordability}</Text>
+      </View>
+      <View style={styles.listContainer}>
+        <View style={styles.subTitleContainer}>
+          <Text style={styles.subTitle}>İçindekiler</Text>
+        </View>
+        <FoodIngredients data={selectedFood.ingredients} />
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 50,
+  },
+
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 5,
+  },
+
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 5,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+    color: "red",
+  },
+  listContainer: {
+    width: "100%",
+    paddingHorizontal: 10,
+  },
+  subTitleContainer: {
+    alignItems: "center",
+    borderBottomWidth: 2,
+    borderBottomColor: "white",
+    marginVertical: 5,
+  },
+  subTitle: {
+    color: "red",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+});
